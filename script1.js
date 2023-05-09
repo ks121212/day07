dab8a9b39a9dd6a7a97eb41c5e2089e12d936108
ks121212 <124970377+ks121212@users.noreply.github.com>
2023-05-09 13:26:19 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
-//1.
+// 1.
 var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v2/all");
+request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
 request.onload=function(){
     var result=JSON.parse(request.response);
@@ -10,26 +10,37 @@ request.onload=function(){
 
 }
 
-//2.
-var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v2/all");
-request.send();
-request.onload=function(){
-    var result=JSON.parse(request.response);
-    console.log(result);
-    var res=result.filter((ele)=>ele.population<200000);
-   console.log(res);
-}
+// 2.
+ var request=new XMLHttpRequest();
+ request.open("GET","https://restcountries.com/v3.1/all");
+ request.send();
+ request.onload=function(){
+     var result=JSON.parse(request.response);
+     console.log(result);
+     var res=result.filter((ele)=>ele.population<200000);
+    console.log(res);
+ }
 
 //3.
 var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v2/all");
-request.send();
-request.onload=function(){
-    var result=JSON.parse(request.response);
-    console.log(result);
-    var res=result.forEach(element => {
-      ele.name,ele.capital,ele.flag
+ request.open("GET","https://restcountries.com/v3.1/all");
+ request.send();
+ request.onload=function(){
+     var result=JSON.parse(request.response);
+     console.log(result);
+     var res=result.forEach(ele =>console.log(ele.name.common,ele.capital,ele.flags.png) );
+     console.log(res);
+ }
+
+// 4.q
+var request=new XMLHttpRequest();
+  request.open("GET","https://restcountries.com/v3.1/all");
+  request.send();
+  request.onload=function(){
+      var result=JSON.parse(request.response);
+      console.log(result);
+     var res=result.filter((ele)=>{ele.currencies.USD
+           console.log(ele.name.common)
     });
-    console.log(res);
-}
+      console.log(res);
+  }
